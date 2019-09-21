81a11c3c653864a168d983d5313b3b153136ecd4
EricHoelscher <40500077+EricHoelscher@users.noreply.github.com>
2019-09-21 14:58:35 -0700

    update robot map

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -29,8 +29,8 @@ public class RobotMap
   public static final int RR_MOTOR = 2;
   public static final int RF_MOTOR = 3;
 
-  public static final int LEAD_BARREL_ANGLE_MOTOR = 4; //TODO
-  public static final int FOLLOW_BARREL_ANGLE_MOTOR = 5; //TODO
+  public static final int LEAD_BARREL_ANGLE_MOTOR = 15; //TODO
+  public static final int FOLLOW_BARREL_ANGLE_MOTOR = 16; //TODO
 
   //Limit swithces
   public static final int UPPER_ARM_SWITCH = 0;
